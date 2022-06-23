7983721ce94bf481a6f4e0cf4d64f6a0018b0c1b
Laura <laura.vivas.gamboa@gmail.com>
2022-06-23 02:12:39 +0200

    changes to the forecast dates to show only day of the week

--- src/components/Forecast5days.vue
+++ src/components/Forecast5days.vue
@@ -31,13 +31,9 @@ export default defineComponent({
       const dates = new Date(timestamp * 1000);
       const dateOptions: object = {
         weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
       };
       const dateLocale = dates.toLocaleDateString('en-US', dateOptions);
       this.date = dateLocale;
-      console.log(this.date)
     },
 
     formatHours(timestamp: any){
